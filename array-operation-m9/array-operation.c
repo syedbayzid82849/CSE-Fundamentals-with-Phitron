896d52289ccf3102a5d64cc3b6a45ef54b6fdd66
syed bayzid <husains82849@gmail.com>
2026-04-19 22:15:06 +0600

    all

--- array-operation-m9/array-operation.c
+++ array-operation-m9/array-operation.c
@@ -24,3 +24,29 @@ int main(){
 }
 
 
+// Insert a value in an array implementation
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n+1];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    int index, value;
+    scanf("%d %d", &index, &value);
+    
+    // shift right
+    for(int i=n; i>index;i--){
+        arr[i] = arr[i-1];
+    }
+    arr[index]=value;
+
+    for (int i=0; i<=n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+
+}
+
+
