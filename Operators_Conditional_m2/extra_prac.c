2357329bf91ce39e899b7cb640057ae3027ca8d5
syed bayzid <husains82849@gmail.com>
2026-04-30 09:47:24 +0600

    i have practice a problem => 'Take a number from user and check if its a positive or negative number.'

--- Operators_Conditional_m2/extra_prac.c
+++ Operators_Conditional_m2/extra_prac.c
@@ -1,18 +1,36 @@
 // Take a number from user and check if its a even number or odd number.
 //input: 5
 //output: 5 is an odd number.
+// #include <stdio.h>
+// int main(){
+//     int num;
+//     scanf("%d", &num);
+//     if(num%2==0){
+//         printf("%d is an even number.", num);
+//     }
+//     else{
+//         printf("%d is an odd number.", num);
+//     }
+// }
+
+
+// Take a number from user and check if its a positive or negative number.
+// input: -5
+// output: -5 is a negative number.
 #include <stdio.h>
 int main(){
     int num;
     scanf("%d", &num);
-    if(num%2==0){
-        printf("%d is an even number.", num);
+    if(num>0){
+        printf("%d is a positive number.", num);
+    }
+    else if(num<0){
+        printf("%d is a negative number.", num);
     }
     else{
-        printf("%d is an odd number.", num);
+        printf("The number is zero.");
     }
 }
 
 
-// Take a number from user and check if its a positive or negative number.
 // Explain if else ladder.
