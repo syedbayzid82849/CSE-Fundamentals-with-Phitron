353da3a1748781a176aecadbe777f7ed8c18d7d3
syed bayzid <husains82849@gmail.com>
2026-05-06 11:17:34 +0600

    Sheet3 - Sorting (Bubble Sort) solved

--- practice_day_1_m14.5/H_Sorting.c
+++ practice_day_1_m14.5/H_Sorting.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 int main() {
-    
+    int N;
+    scanf("%d", &N);
+    int A[N];
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+     for (int i = 0; i < N-1; i++) {
+        for(int j=i+1; j<N; j++){
+            if(A[i] > A[j]){
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        printf("%d ", A[i]);
+    }
     return 0;
 }
