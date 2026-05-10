fca2f7e8c036893de188617a8e45d2ec252cee46
syed bayzid <husains82849@gmail.com>
2026-05-10 22:26:52 +0600

    print 1 to 10 using recursion

--- recurison_m17/recursion_and_control.c
+++ recurison_m17/recursion_and_control.c
@@ -1,11 +1,28 @@
+// #include <stdio.h>
+// void recursive_function(int n)
+// {
+//     printf("%d\n", n);      
+//     recursive_function(n);
+// }
+// int main()
+// {
+//     recursive_function(5);
+//     return 0;
+// }
+
+//print 1 to 10 using recursion
 #include <stdio.h>
-void recursive_function(int n)
-{
-    printf("%d\n", n);      
-    recursive_function(n);
+
+void recursive_print(int n) {
+    if (n > 10) {
+        return; // Base case: stop recursion when n exceeds 10
+    }
+    printf("%d\n", n); // Print the current value of n
+    recursive_print(n + 1); // Recursive call with the next value
 }
-int main()
-{
-    recursive_function(5);
+
+int main() {
+    int n = 1;
+    recursive_print(n);
     return 0;
 }
